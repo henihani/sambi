2f630617dd708958f4f47e1a408f79feb7dd9cf4
Chudlori A <nchudlori@gmail.com>
2020-10-13 01:51:37 +0700

    Tersedia tidak tersedia

--- app/Http/Controllers/PencarianController.php
+++ app/Http/Controllers/PencarianController.php
@@ -2,32 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
+use App\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Book;
 
 class PencarianController extends Controller
 {
     public function index()
     {
-        $books = Book::all();
-        return view ('layouts.pencarian',compact('books','books'));
+        // $books = Book::all();
+        $books = Book::whereNotIn('id', Status::select('books_id')->get())->get();
+        return view('layouts.pencarian', compact('books', 'books'));
     }
     public function cari(Request $request)
-	{
-		// menangkap data pencarian
-		$cari = $request->cari;
- 
-    		// mengambil data dari table pegawai sesuai pencarian data
-		$books = DB::table('books')
-        ->where('pengarang','like',"%".$cari."%")
-        ->orWhere('judul','like',"%".$cari."%")
-        ->orWhere('penerbit','like',"%".$cari."%")
-        ->orWhere('kelas','like',"%".$cari."%")
-        ->orWhere('semester','like',"%".$cari."%")
-		->get();
- 
-    		// mengirim data pegawai ke view index
-        return view('layouts.pencarian',['books' => $books]);
+    {
+        // menangkap data pencarian
+        $cari = $request->cari;
+
+        // mengambil data dari table pegawai sesuai pencarian data
+        $books = DB::table('books')
+            ->where('pengarang', 'like', "%" . $cari . "%")
+            ->orWhere('judul', 'like', "%" . $cari . "%")
+            ->orWhere('penerbit', 'like', "%" . $cari . "%")
+            ->orWhere('kelas', 'like', "%" . $cari . "%")
+            ->orWhere('semester', 'like', "%" . $cari . "%")
+            ->get();
+
+        // mengirim data pegawai ke view index
+        return view('layouts.pencarian', ['books' => $books]);
     }
 }
